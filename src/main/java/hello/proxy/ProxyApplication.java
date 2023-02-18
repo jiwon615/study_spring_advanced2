bbdd831fc04e8a19f6ece422edcd395e42aec8ae
jiwon615 <seg615@naver.com>
2023-02-18 19:08:02 +0900

    feat: v2 인터페이스 없는 구체 클래스 - 스프링 빈으로 수동 등록

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,11 +1,12 @@
 package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class)
+@Import({AppV1Config.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") // 주의 (특정 패키지 안적으면 기본적으로 hello.proxy 하위패키지를 componentScan 함)
 public class ProxyApplication {
 
