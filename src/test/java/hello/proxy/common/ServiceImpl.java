ae1cec13de1455f7d8e1350142013eed386b654d
jiwon615 <seg615@naver.com>
2023-02-22 21:10:10 +0900

    test: ProxyFactory를 통해 JDK 동적 프록시 사용

--- src/test/java/hello/proxy/common/ServiceImpl.java
+++ src/test/java/hello/proxy/common/ServiceImpl.java
@@ -3,7 +3,7 @@ package hello.proxy.common;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class ServinceImpl implements ServiceInterface{
+public class ServiceImpl implements ServiceInterface{
     @Override
     public void save() {
         log.info("save 호출");
---
+++ src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
@@ -0,0 +1,34 @@
+package hello.proxy.proxyfactory;
+
+import hello.proxy.common.ServiceImpl;
+import hello.proxy.common.ServiceInterface;
+import hello.proxy.common.advice.TimeAdvice;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.AopUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class ProxyFactoryTest {
+
+    @Test
+    @DisplayName("인터페이스가 있으면 JDK 동적 프록시 사용")
+    void interfaceProxy() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy(); // 프록시 객체를 생성하고 그 결과를 받음
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.save();
+
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+    }
+}
